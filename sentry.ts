10c4ed614ab94fd074aa849285e077400a0227b6
Hong Minhee <hong@minhee.org>
2024-11-21 00:57:56 +0900

    Log Sentry initialization

--- sentry.ts
+++ sentry.ts
@@ -1,10 +1,15 @@
+import "./logging.ts";
+import { getLogger } from "@logtape/logtape";
 import {
   denoCronIntegration,
   getDefaultIntegrations,
   init,
 } from "@sentry/deno";
 
+const logger = getLogger(["hackerspub", "sentry"]);
+
 const SENTRY_DSN = Deno.env.get("SENTRY_DSN");
+
 if (SENTRY_DSN != null) {
   init({
     dsn: SENTRY_DSN,
@@ -14,4 +19,9 @@ if (SENTRY_DSN != null) {
       denoCronIntegration(),
     ],
   });
+  logger.debug("Sentry initialized.", { dsn: SENTRY_DSN });
+} else {
+  logger.debug(
+    "Sentry not initialized: missing SENTRY_DSN environment variable.",
+  );
 }
