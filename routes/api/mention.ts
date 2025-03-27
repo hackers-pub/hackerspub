24b4949b4de58dc281fd3570322af6e0eb11b9a5
Hong Minhee <hong@minhee.org>
2025-03-27 12:26:22 +0900

    Exclude instance actor from mention search results

--- routes/api/mention.ts
+++ routes/api/mention.ts
@@ -15,17 +15,24 @@ export const handler = define.handlers({
       : [prefix, undefined];
     const canonicalHost = new URL(ctx.state.fedCtx.canonicalOrigin).host;
     const result: Actor[] = await db.query.actorTable.findMany({
-      where: host == null || !URL.canParse(`http://${host}`)
-        ? { username: { ilike: `${username}%` } }
-        : {
-          username,
-          handleHost: {
-            ilike: `${new URL(`http://${host}`).host}%`,
-          },
-          instanceHost: {
-            ilike: `${new URL(`http://${host}`).host}%`,
-          },
+      where: {
+        ...(host == null || !URL.canParse(`http://${host}`)
+          ? { username: { ilike: `${username}%` } }
+          : {
+            username,
+            handleHost: {
+              ilike: `${new URL(`http://${host}`).host}%`,
+            },
+            instanceHost: {
+              ilike: `${new URL(`http://${host}`).host}%`,
+            },
+          }),
+        // Exclude the instance actor:
+        NOT: {
+          username: canonicalHost,
+          handleHost: canonicalHost,
         },
+      },
       orderBy: (t) => [
         desc(eq(t.username, username)),
         desc(eq(t.handleHost, canonicalHost)),
