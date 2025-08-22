bb0fce4428b18876ec3b7e6e2fdfd1ea8939b125
Haze Lee <hazelee@realignist.me>
2025-08-22 17:45:45 +0900

    feat(web, wip): visibility config preference
    - add `DefaultVisibilityPreference` island for configure default visibility
    - update locales for new feature (en, ja, ko)

--- web/routes/@[username]/settings/preferences.tsx
+++ web/routes/@[username]/settings/preferences.tsx
@@ -3,9 +3,12 @@ import { accountTable } from "@hackerspub/models/schema";
 import { eq } from "drizzle-orm";
 import { Button } from "../../../components/Button.tsx";
 import { Msg } from "../../../components/Msg.tsx";
+import { Label } from "../../../components/Label.tsx";
 import { SettingsNav } from "../../../components/SettingsNav.tsx";
 import { db } from "../../../db.ts";
 import { define } from "../../../utils.ts";
+import type { PostVisibility } from "@hackerspub/models/schema";
+import { DefaultVisibilityPreference } from "../../../islands/DefaultVisibilityPreference.tsx";
 
 export const handler = define.handlers({
   GET(ctx) {
@@ -24,8 +27,11 @@ export const handler = define.handlers({
     }
     const form = await ctx.req.formData();
     const preferAiSummary = form.get("preferAiSummary") === "true";
+    const noteVisibility = form.get("noteVisibility") as PostVisibility;
+    const shareVisibility = form.get("shareVisibility") as PostVisibility;
+
     const accounts = await db.update(accountTable)
-      .set({ preferAiSummary })
+      .set({ preferAiSummary, postVisibility, shareVisibility })
       .where(eq(accountTable.id, ctx.state.account.id))
       .returning();
     return page<PreferencesPageProps>(accounts[0]);
@@ -35,33 +41,42 @@ export const handler = define.handlers({
 interface PreferencesPageProps {
   preferAiSummary: boolean;
   leftInvitations: number;
+  postVisibility: PostVisibility;
+  shareVisibility: PostVisibility;
 }
 
 export default define.page<typeof handler, PreferencesPageProps>((
-  { data, params },
-) => (
-  <div>
-    <SettingsNav
-      active="preferences"
-      settingsHref={`/@${params.username}/settings`}
-      leftInvitations={data.leftInvitations}
-    />
-    <form method="post" class="mt-4">
-      <label>
-        <input
-          type="checkbox"
-          name="preferAiSummary"
-          checked={data.preferAiSummary}
-          value="true"
-        />{" "}
-        <Msg $key="settings.preferences.preferAiSummary" />
-      </label>
-      <p class="opacity-50">
-        <Msg $key="settings.preferences.preferAiSummaryDescription" />
-      </p>
-      <Button type="submit" class="mt-4">
-        <Msg $key="settings.preferences.save" />
-      </Button>
-    </form>
-  </div>
-));
+  { state: { language, t }, data, params },
+) => {
+  return (
+    <div>
+      <SettingsNav
+        active="preferences"
+        settingsHref={`/@${params.username}/settings`}
+        leftInvitations={data.leftInvitations}
+      />
+      <form method="post" class="mt-4">
+        <Label label={t("settings.preferences.preferAiSummary")}>
+          <input
+            type="checkbox"
+            name="preferAiSummary"
+            checked={data.preferAiSummary}
+            value="true"
+          />{" "}
+          <Msg $key="settings.preferences.preferAiSummary" />
+          <p class="opacity-50">
+            <Msg $key="settings.preferences.preferAiSummaryDescription" />
+          </p>
+        </Label>
+        <DefaultVisibilityPreference
+          language={language}
+          noteVisibility={data.noteVisibility}
+          shareVisibility={data.shareVisibility}
+        />
+        <Button type="submit" class="mt-4">
+          <Msg $key="settings.preferences.save" />
+        </Button>
+      </form>
+    </div>
+  );
+});
