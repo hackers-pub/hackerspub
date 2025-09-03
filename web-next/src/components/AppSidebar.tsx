41ea1f40414754a8d19389fc1550aa5ee8fed48d
Hong Minhee <hong@minhee.org>
2025-09-03 22:20:12 +0900

    web-next: Add invite menu item to sidebar
    Display invite link in sidebar when user has invitations available.
    Shows invitation count and links to invitation settings page.

--- web-next/src/components/AppSidebar.tsx
+++ web-next/src/components/AppSidebar.tsx
@@ -60,6 +60,7 @@ export function AppSidebar(props: AppSidebarProps) {
         name
         username
         avatarUrl
+        invitationsLeft
       }
     `,
     () => props.$signedAccount,
@@ -261,6 +262,33 @@ export function AppSidebar(props: AppSidebarProps) {
                       {signedAccount().name}
                     </SidebarMenuButton>
                   </SidebarMenuItem>
+                  <Show when={signedAccount().invitationsLeft > 0}>
+                    <SidebarMenuItem class="list-none">
+                      <SidebarMenuButton
+                        as={A}
+                        href={`/@${signedAccount().username}/settings/invite`}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          class="size-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                          />
+                        </svg>
+                        {t`Invite`}
+                        <span class="text-xs text-muted-foreground">
+                          ({signedAccount().invitationsLeft})
+                        </span>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  </Show>
                   <SidebarMenuItem class="list-none">
                     <SidebarMenuButton
                       as={A}
