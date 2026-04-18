843f181ff9308f9a1fe39590ee85bc3b84958961
malkoG <rijgndqw012@gmail.com>
2026-04-18 13:38:48 +0900

    Squash migration files

--- drizzle/0090_bookmark.sql
+++ drizzle/0090_bookmark.sql
@@ -7,5 +7,5 @@ CREATE TABLE "bookmark" (
 --> statement-breakpoint
 ALTER TABLE "bookmark" ADD CONSTRAINT "bookmark_account_id_account_id_fk" FOREIGN KEY ("account_id") REFERENCES "public"."account"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "bookmark" ADD CONSTRAINT "bookmark_post_id_post_id_fk" FOREIGN KEY ("post_id") REFERENCES "public"."post"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "idx_bookmark_account_created" ON "bookmark" USING btree ("account_id","created" desc);--> statement-breakpoint
+CREATE INDEX "idx_bookmark_account_created" ON "bookmark" USING btree ("account_id","created" desc,"post_id" desc);--> statement-breakpoint
 CREATE INDEX "bookmark_post_id_index" ON "bookmark" USING btree ("post_id");
--- drizzle/0091_bookmark_index_postid.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX "idx_bookmark_account_created";--> statement-breakpoint
-CREATE INDEX "idx_bookmark_account_created" ON "bookmark" USING btree ("account_id","created" desc,"post_id" desc);
